252445dc07d0f4f015f2a93048233e6dbce37cc2
pk <pk@example.com>
2025-03-08 23:29:16 +0530

    new chnages

--- test/PetAdoption.js
+++ test/PetAdoption.js
@@ -1,10 +1,51 @@
 const { expect } = require("chai");
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("PetAdoption", function() {
 
+  async function deployContractFixture() {
+    const [owner] = await ethers.getSigners();
+    const PetAdoption = await ethers.getContractFactory("PetAdoption");
+    const contract = await PetAdoption.deploy();
+
+  
+
+    return { owner, contract };
+  }
+
   describe("Deployment", function() {
-    it("Should set the right owner", function() {
-      expect(true).to.equal(true);
+    it("Should set the right owner", async function() {
+
+
+        const { owner, contract } = await loadFixture(deployContractFixture);
+        
+        const contractOwner = await contract.owner();
+ 
+       expect(contractOwner).to.equal(owner.address);
+
+      
     });
+    it("Should get the right owner", async function() {
+
+
+      const { owner, contract } = await loadFixture(deployContractFixture);
+      
+      const contractOwner = await contract.getOwner();
+
+     expect(contractOwner).to.equal(owner.address);
+
+    
   });
+  });
+
+  describe("Add Pet", function() {
+    it("Should revert with the right error in case of other account", async function() {
+      const { owner, contract, account2 } = await loadFixture(deployContractFixture);
+
+      await expect(contract.connect(account2).addPet()).to.be.revertedWith("Only a contract owner can add a new pet!");
+    })
+  })
+
 });
+
+// npx hardhat test --network localhost
