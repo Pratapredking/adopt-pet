20101b0186a5555ffd2198dc54039f4307879caf
pk <pk@example.com>
2025-03-29 20:54:48 +0530

    new file

--- test/PetAdoption.js
+++ test/PetAdoption.js
@@ -1,59 +1,68 @@
 const { expect } = require("chai");
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 
-describe("PetAdoption", function() {
+describe("PetAdoption", function () {
 
   async function deployContractFixture() {
-    
+
     const PETS_COUNT = 5;
     const [owner, account2] = await ethers.getSigners();
     const PetAdoption = await ethers.getContractFactory("PetAdoption");
     const contract = await PetAdoption.deploy(PETS_COUNT);
 
-  
+    return { owner, contract, account2, petsAddedCount: PETS_COUNT };
 
-    return { owner, contract, account2,petsAddedCount: PETS_COUNT  };
+
   }
 
-  describe("Deployment", function() {
-    it("Should set the right owner", async function() {
+  describe("Deployment", function () {
+    it("Should set the right owner", async function () {
+
+      const { owner, contract } = await loadFixture(deployContractFixture);
+
+      const contractOwner = await contract.owner();
+
+      expect(contractOwner).to.equal(owner.address);
 
 
-        const { owner, contract } = await loadFixture(deployContractFixture);
-        
-        const contractOwner = await contract.owner();
- 
-       expect(contractOwner).to.equal(owner.address);
-
-      
     });
-    it("Should get the right owner", async function() {
+    it("Should get the right owner", async function () {
 
 
       const { owner, contract } = await loadFixture(deployContractFixture);
-      
+
       const contractOwner = await contract.getOwner();
 
-     expect(contractOwner).to.equal(owner.address);
+      expect(contractOwner).to.equal(owner.address);
 
-    
-  });
+
+    });
   });
 
-  describe("Add Pet", function() {
-    it("Should revert with the right error in case of other account", async function() {
+  describe("Add Pet", function () {
+    it("Should revert with the right error in case of other account", async function () {
       const { contract, account2 } = await loadFixture(deployContractFixture);
 
       await expect(contract.connect(account2).addPet()).to.be.revertedWith("Only a contract owner can add a new pet!");
     });
-    it("Should increase Pet index", async function() {
+    it("Should increase Pet index", async function () {
       const { contract, petsAddedCount } = await loadFixture(deployContractFixture);
 
       await contract.addPet();
 
       expect(await contract.petIndex()).to.equal(petsAddedCount + 1)
     })
-  
+
+  })
+
+  describe("Adopt pet", function() {
+    it("Should revert with index out of bounds", async function() {
+      const { contract, petsAddedCount } = await loadFixture(deployContractFixture);
+    
+      
+      await expect(contract.adoptPet(petsAddedCount)).to.be.revertedWith("Pet index out of bound!");
+      await expect(contract.adoptPet(-1)).to.be.rejectedWith("value out-of-bounds");
+    });
   })
 
 });
