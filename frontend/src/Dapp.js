5a5fb3e0cbf85d927c646a55b02ea1856073436c
pk <pk@example.com>
2025-04-06 10:39:16 +0530

    add

--- frontend/src/Dapp.js
+++ frontend/src/Dapp.js
@@ -1,16 +1,41 @@
+import { useState, useEffect } from "react";
+
 import { Navbar } from "./components/Navbar";
 import { PetItem } from "./components/PetItem";
 import { TxError } from "./components/TxError";
+import { WalletNotDetected } from "./components/WalletNotDetected";
 
 function Dapp() {
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    async function fetchPets() {
+      const res = await fetch("/pets.json");
+      const data = await res.json();
+      setPets(data);
+    } 
+
+    fetchPets();
+  }, []);
+
+  if (window.ethereum === undefined) {
+    return <WalletNotDetected />
+  }
+
   return (
     <div className="container">
      <TxError/>
       <br/>
       
       <Navbar/>   
+
+
+     
       <div className="items">
-        <PetItem/>
+      { 
+        pets.map((pet) =>
+        <PetItem key={pet.id} pet={pet}/>
+      )}
       </div>
       
     </div>
